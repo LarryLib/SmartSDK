45959ff2aabbc8475df9282d76959806690b5ffc
LarryMacPro <lxd_super_main@126.com>
2023-08-24 11:40:01 +0800

    v0.0.1

---
+++ Classes/Funcs.swift
@@ -0,0 +1,20 @@
+//
+//   Funcs.swift
+//   aaa
+//
+//   Created by LMP
+    
+
+import Foundation
+
+public func sum(m: Int, n: Int) -> Int {
+    return m + n
+}
+
+func sum2(m: Int, n: Int) -> Int {
+    return (m + n) * 2
+}
+
+private func sum3(m: Int, n: Int) -> Int {
+    return (m + n) * 3
+}
